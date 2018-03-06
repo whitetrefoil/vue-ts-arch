99324cb47a73355c486599108399eeb5947a315b
WhiteTrefoil <whitetrefoil@gmail.com>
2018-03-06 10:32:52 +0800

    Upgrade to official @types/webpack

--- declarations/temporary/webpack.d.ts
+++
@@ -1,9 +0,0 @@
-// tslint:disable:no-implicit-dependencies
-
-import * as webpack from 'webpack'
-
-declare module 'webpack' {
-  export interface Configuration {
-    mode?: 'development'|'production'
-  }
-}
